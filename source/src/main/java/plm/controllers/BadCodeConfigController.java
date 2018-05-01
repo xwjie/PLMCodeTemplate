47deb6a25c6821980005929f8e2530f5728d3871
xwjie <121509092@qq.com>
2018-05-01 22:36:38 +0800

    整理格式

--- source/src/main/java/plm/controllers/BadCodeConfigController.java
+++ source/src/main/java/plm/controllers/BadCodeConfigController.java
@@ -30,127 +30,131 @@ import plm.services.ConfigService2;
 @RequestMapping("/config2")
 @RestController
 public class BadCodeConfigController {
-	//
-	@Autowired
-	ConfigService2 configService;
+  //
+  @Autowired
+  ConfigService2 configService;
 
-	private static final Logger log = LoggerFactory.getLogger(ConfigService2.class);
+  private static final Logger log = LoggerFactory
+      .getLogger(ConfigService2.class);
 
-	/**
-	 * 成功返回boolean，失败返回string，大忌 出现无关的参数：lang
-	 * 
-	 * @param id
-	 * @param lang
-	 * @return
-	 */
-	@PostMapping("/delete2")
-	public Object delete2(long id, String lang) {
-		try {
-			// 语言（中英文提示不同）
-			Locale local = "zh".equalsIgnoreCase(lang) ? Locale.CHINESE : Locale.ENGLISH;
-			boolean result = configService.delete(id, local);
-			return result;
-		} catch (Exception e) {
-			log.error("delete error", e);
-			return e.toString();
-		}
-	}
+  /**
+   * 成功返回boolean，失败返回string，大忌 出现无关的参数：lang
+   * 
+   * @param id
+   * @param lang
+   * @return
+   */
+  @PostMapping("/delete2")
+  public Object delete2(long id, String lang) {
+    try {
+      // 语言（中英文提示不同）
+      Locale local = "zh".equalsIgnoreCase(lang) ? Locale.CHINESE
+          : Locale.ENGLISH;
+      boolean result = configService.delete(id, local);
+      return result;
+    } catch (Exception e) {
+      log.error("delete error", e);
+      return e.toString();
+    }
+  }
 
-	/**
-	 * 错误范例，输入输出都是复杂参数
-	 * 
-	 * @param params
-	 * @return
-	 */
-	@PostMapping("/add")
-	public Map<String, Object> add(Map<String, Object> params) {
-		Map<String, Object> data = new HashMap<String, Object>();
+  /**
+   * 错误范例，输入输出都是复杂参数
+   * 
+   * @param params
+   * @return
+   */
+  @PostMapping("/add")
+  public Map<String, Object> add(Map<String, Object> params) {
+    Map<String, Object> data = new HashMap<String, Object>();
 
-		boolean result = false;
+    boolean result = false;
 
-		try {
-			// 语言（中英文提示不同）
-			result = configService.add(params);
-			data.put("code", 0);
-		} catch (CheckException e) {
-			// 参数等校验出错，已知异常，不需要打印堆栈，返回码为-1
-			data.put("code", -1);
-			data.put("msg", e.getMessage());
-		} catch (Exception e) {
-			// 其他未知异常，需要打印堆栈分析用，返回码为99
-			log.error("add config error", e);
+    try {
+      // 语言（中英文提示不同）
+      result = configService.add(params);
+      data.put("code", 0);
+    } catch (CheckException e) {
+      // 参数等校验出错，已知异常，不需要打印堆栈，返回码为-1
+      data.put("code", -1);
+      data.put("msg", e.getMessage());
+    } catch (Exception e) {
+      // 其他未知异常，需要打印堆栈分析用，返回码为99
+      log.error("add config error", e);
 
-			data.put("code", 99);
-			data.put("msg", e.toString());
-		}
+      data.put("code", 99);
+      data.put("msg", e.toString());
+    }
 
-		data.put("result", result);
+    data.put("result", result);
 
-		return data;
-	}
+    return data;
+  }
 
-	/**
-	 * 错误范例，输入json格式复杂参数，没有考虑失败
-	 * 
-	 * @param jsonStr
-	 * @return
-	 */
-	@PostMapping("/update")
-	public void update(long id, String jsonStr) {
+  /**
+   * 错误范例，输入json格式复杂参数，没有考虑失败
+   * 
+   * @param jsonStr
+   * @return
+   */
+  @PostMapping("/update")
+  public void update(long id, String jsonStr) {
 
-		JSONObject params = JSON.parseObject(jsonStr);
+    JSONObject params = JSON.parseObject(jsonStr);
 
-		// 其他操作
-	}
+    // 其他操作
+  }
 
-	/**
-	 * 根据id删除对象
-	 * 
-	 * @param id
-	 * @param lang
-	 * @return
-	 */
-	@PostMapping("/delete")
-	public Map<String, Object> delete(long id, String lang) {
-		Map<String, Object> data = new HashMap<String, Object>();
+  /**
+   * 根据id删除对象
+   * 
+   * @param id
+   * @param lang
+   * @return
+   */
+  @PostMapping("/delete")
+  public Map<String, Object> delete(long id, String lang) {
+    Map<String, Object> data = new HashMap<String, Object>();
 
-		boolean result = false;
-		try {
-			// 语言（中英文提示不同）
-			Locale local = "zh".equalsIgnoreCase(lang) ? Locale.CHINESE : Locale.ENGLISH;
+    boolean result = false;
+    try {
+      // 语言（中英文提示不同）
+      Locale local = "zh".equalsIgnoreCase(lang) ? Locale.CHINESE
+          : Locale.ENGLISH;
 
-			result = configService.delete(id, local);
+      result = configService.delete(id, local);
 
-			data.put("code", 0);
+      data.put("code", 0);
 
-		} catch (CheckException e) {
-			// 参数等校验出错，已知异常，不需要打印堆栈，返回码为-1
-			data.put("code", -1);
-			data.put("msg", e.getMessage());
-		} catch (Exception e) {
-			// 其他未知异常，需要打印堆栈分析用，返回码为99
-			log.error("delete config error", e);
+    } catch (CheckException e) {
+      // 参数等校验出错，已知异常，不需要打印堆栈，返回码为-1
+      data.put("code", -1);
+      data.put("msg", e.getMessage());
+    } catch (Exception e) {
+      // 其他未知异常，需要打印堆栈分析用，返回码为99
+      log.error("delete config error", e);
 
-			data.put("code", 99);
-			data.put("msg", e.toString());
-		}
+      data.put("code", 99);
+      data.put("msg", e.toString());
+    }
 
-		data.put("result", result);
+    data.put("result", result);
 
-		return data;
-	}
+    return data;
+  }
 
-	/**
-	 * ！！！错误规范 增加配置
-	 */
-	@PostMapping("/add2")
-	public void add2(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.setContentType("application/json;charset=UTF-8");
+  /**
+   * ！！！错误规范 增加配置
+   */
+  @PostMapping("/add2")
+  public void add2(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("application/json;charset=UTF-8");
 
-		// 返回了一个json格式的字符串。。。
-		String result = configService.add(request);
+    // 返回了一个json格式的字符串。。。
+    String result = configService.add(request);
 
-		response.getOutputStream().write(result.getBytes());
-		response.getOutputStream().flush();
-	}
+    response.getOutputStream().write(result.getBytes());
+    response.getOutputStream().flush();
+  }
 }
--- source/src/main/java/plm/services/ConfigService2.java
+++ source/src/main/java/plm/services/ConfigService2.java
@@ -28,213 +28,214 @@ import plm.daos.ConfigDao;
 /**
  * ！！！错误代码示例
  * 
- * @author 肖文杰  https://xwjie.github.io/PLMCodeTemplate/
+ * @author 肖文杰 https://xwjie.github.io/PLMCodeTemplate/
  *
  */
 @Service
 public class ConfigService2 {
 
-	private static final Logger logger = LoggerFactory.getLogger(ConfigService2.class);
+  private static final Logger logger = LoggerFactory
+      .getLogger(ConfigService2.class);
 
-	@Autowired
-	ConfigDao dao;
+  @Autowired
+  ConfigDao dao;
 
-	@Autowired
-	MessageSource messageSource;
+  @Autowired
+  MessageSource messageSource;
 
-	public Collection<Config> getAll() {
-		return dao.getAll();
-	}
+  public Collection<Config> getAll() {
+    return dao.getAll();
+  }
 
-	/**
-	 * ！！！错误示范
-	 * 
-	 * 出现和业务无关的参数local
-	 * 
-	 * @param id
-	 * @param locale
-	 * @return
-	 */
-	public boolean delete(long id, Locale locale) {
-		// 参数校验
-		if (id <= 0L) {
-			if (locale.equals(Locale.CHINESE)) {
-				throw new CheckException("非法的ID：" + id);
-			} else {
-				throw new CheckException("Illegal ID:" + id);
-			}
-		}
+  /**
+   * ！！！错误示范
+   * 
+   * 出现和业务无关的参数local
+   * 
+   * @param id
+   * @param locale
+   * @return
+   */
+  public boolean delete(long id, Locale locale) {
+    // 参数校验
+    if (id <= 0L) {
+      if (locale.equals(Locale.CHINESE)) {
+        throw new CheckException("非法的ID：" + id);
+      } else {
+        throw new CheckException("Illegal ID:" + id);
+      }
+    }
 
-		boolean result = dao.delete(id);
+    boolean result = dao.delete(id);
 
-		// 修改操作需要打印操作结果
-		logger.info("delete config success, id:" + id + ", result:" + result);
+    // 修改操作需要打印操作结果
+    logger.info("delete config success, id:" + id + ", result:" + result);
 
-		return dao.delete(id);
-	}
+    return dao.delete(id);
+  }
 
-	public void doSome() {
+  public void doSome() {
 
-	}
+  }
 
-	/**
-	 * ！！！错误的示例
-	 * 
-	 * @param request
-	 * @return
-	 */
-	public String add(HttpServletRequest request) {
-		Map<String, Object> data = new HashMap<String, Object>();
+  /**
+   * ！！！错误的示例
+   * 
+   * @param request
+   * @return
+   */
+  public String add(HttpServletRequest request) {
+    Map<String, Object> data = new HashMap<String, Object>();
 
-		try {
-			String name = (String) request.getParameter("name");
-			String value = (String) request.getParameter("value");
+    try {
+      String name = (String) request.getParameter("name");
+      String value = (String) request.getParameter("value");
 
-			// 示例代码
-			long newID = add(name, value);
+      // 示例代码
+      long newID = add(name, value);
 
-			data.put("code", 0);
-			data.put("newID", newID);
-		} catch (CheckException e) {
-			// 参数等校验出错，已知异常，不需要打印堆栈，返回码为-1
-			data.put("code", -1);
-			data.put("msg", e.getMessage());
-		} catch (Exception e) {
-			// 其他未知异常，需要打印堆栈分析用，返回码为99
-			logger.error("add config error", e);
+      data.put("code", 0);
+      data.put("newID", newID);
+    } catch (CheckException e) {
+      // 参数等校验出错，已知异常，不需要打印堆栈，返回码为-1
+      data.put("code", -1);
+      data.put("msg", e.getMessage());
+    } catch (Exception e) {
+      // 其他未知异常，需要打印堆栈分析用，返回码为99
+      logger.error("add config error", e);
 
-			data.put("code", 99);
-			data.put("msg", e.toString());
-		}
+      data.put("code", 99);
+      data.put("msg", e.toString());
+    }
 
-		return JSONObject.toJSONString(data);
-	}
+    return JSONObject.toJSONString(data);
+  }
 
-	private long add(String name, String value) {
-		logger.info("add config ,name:" + name + ", value:" + value);
-		// XXX
-		return 1L;
-	}
+  private long add(String name, String value) {
+    logger.info("add config ,name:" + name + ", value:" + value);
+    // XXX
+    return 1L;
+  }
 
-	/**
-	 * ！！！错误代码示例
-	 * 
-	 * 1. 和业务无关的参数locale，messagesource 2. 输入输出都是map，根本不知道输入了什么，返回了什么
-	 * 
-	 * @param params
-	 * @param local
-	 * @param messageSource
-	 * @return
-	 */
-	public Map<String, Object> addConfig(Map<String, Object> params) {
+  /**
+   * ！！！错误代码示例
+   * 
+   * 1. 和业务无关的参数locale，messagesource 2. 输入输出都是map，根本不知道输入了什么，返回了什么
+   * 
+   * @param params
+   * @param local
+   * @param messageSource
+   * @return
+   */
+  public Map<String, Object> addConfig(Map<String, Object> params) {
 
-		Map<String, Object> data = new HashMap<String, Object>();
+    Map<String, Object> data = new HashMap<String, Object>();
 
-		// 语言（中英文提示不同）
-		Locale locale = null;
+    // 语言（中英文提示不同）
+    Locale locale = null;
 
-		try {
-			String lang = (String) params.get("lang");
-			locale = "zh".equalsIgnoreCase(lang) ? Locale.CHINESE : Locale.ENGLISH;
+    try {
+      String lang = (String) params.get("lang");
+      locale = "zh".equalsIgnoreCase(lang) ? Locale.CHINESE : Locale.ENGLISH;
 
-			String name = (String) params.get("name");
-			String value = (String) params.get("value");
+      String name = (String) params.get("name");
+      String value = (String) params.get("value");
 
-			// 示例代码，省略其他代码
-		} catch (Exception e) {
-			logger.error("add config error", e);
+      // 示例代码，省略其他代码
+    } catch (Exception e) {
+      logger.error("add config error", e);
 
-			data.put("code", 99);
-			data.put("msg", messageSource.getMessage("SYSTEMERROR", null, locale));
-		}
+      data.put("code", 99);
+      data.put("msg", messageSource.getMessage("SYSTEMERROR", null, locale));
+    }
 
-		return data;
-	}
+    return data;
+  }
 
-	public void updateUser(Map<String, Object> params) {
-		long userId = (Long) params.get("id");
-		String nickname = (String) params.get("nickname");
+  public void updateUser(Map<String, Object> params) {
+    long userId = (Long) params.get("id");
+    String nickname = (String) params.get("nickname");
 
-		// 更新代码
-	}
+    // 更新代码
+  }
 
-	public void updateUserNickName(long userId, String nickname) {
-		// 更新代码
-	}
+  public void updateUserNickName(long userId, String nickname) {
+    // 更新代码
+  }
 
-	public boolean add(Map<String, Object> params) {
-		String name = (String) params.get("name");
-		String value = (String) params.get("value");
+  public boolean add(Map<String, Object> params) {
+    String name = (String) params.get("name");
+    String value = (String) params.get("value");
 
-		// 执行新增操作
+    // 执行新增操作
 
-		return true;
-	}
+    return true;
+  }
 
-	/**
-	 * !!错误范例：随便捕获异常加空判断，代码最大的隐患
-	 * 
-	 * @param id
-	 * @param value
-	 */
-	public void updateConfigValue(long id, String value) {
-		Config config = null;
+  /**
+   * !!错误范例：随便捕获异常加空判断，代码最大的隐患
+   * 
+   * @param id
+   * @param value
+   */
+  public void updateConfigValue(long id, String value) {
+    Config config = null;
 
-		try {
-			config = findConfigById(id);
-		} catch (Exception e) {
-			logger.error("findConfigByName error", e);
-		}
+    try {
+      config = findConfigById(id);
+    } catch (Exception e) {
+      logger.error("findConfigByName error", e);
+    }
 
-		if (config != null) {
-			config.setValue(value);
+    if (config != null) {
+      config.setValue(value);
 
-			// 执行数据库保存操作
-			doSave(config);
-		}
-	}
+      // 执行数据库保存操作
+      doSave(config);
+    }
+  }
 
-	/**
-	 * 正确写法
-	 * 
-	 * @param name
-	 * @param value
-	 */
-	public void updateConfigValue2(long id, String value) {
-		Config config = findConfigById(id);
+  /**
+   * 正确写法
+   * 
+   * @param name
+   * @param value
+   */
+  public void updateConfigValue2(long id, String value) {
+    Config config = findConfigById(id);
 
-		config.setValue(value);
+    config.setValue(value);
 
-		// 执行数据库保存操作
-		doSave(config);
-	}
+    // 执行数据库保存操作
+    doSave(config);
+  }
 
-	private void doSave(Config config) {
-		// TODO Auto-generated method stub
-	}
+  private void doSave(Config config) {
+    // TODO Auto-generated method stub
+  }
 
-	private Config findConfigById(long id) throws CheckException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  private Config findConfigById(long id) throws CheckException {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	/**
-	 * 错误演示：配置代码和业务代码耦合
-	 */
-	public void someServiceCode() {
-		// 从某个地方得到配置的json对象
-		JSONObject configJson = readConfig();
+  /**
+   * 错误演示：配置代码和业务代码耦合
+   */
+  public void someServiceCode() {
+    // 从某个地方得到配置的json对象
+    JSONObject configJson = readConfig();
 
-		// 如果某个配置项为true
-		if (configJson.getBooleanValue("someFlag")) {
-			// 执行某些操作
-		}
+    // 如果某个配置项为true
+    if (configJson.getBooleanValue("someFlag")) {
+      // 执行某些操作
+    }
 
-	}
+  }
 
-	private JSONObject readConfig() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  private JSONObject readConfig() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
 }
