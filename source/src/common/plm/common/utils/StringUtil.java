73772327b0337d7648708410badae4f6d45d17a6
xwjie <121509092@qq.com>
2018-05-19 19:08:48 +0800

    用bootstrap重新了首页

--- source/src/common/plm/common/utils/StringUtil.java
+++ source/src/common/plm/common/utils/StringUtil.java
@@ -1,10 +1,10 @@
 package plm.common.utils;
 
-import java.util.Collection;
+		import java.util.Collection;
 
 /**
  * 工具类的规范例子
- * 
+ *
  * @author  晓风轻 https://xwjie.github.io/PLMCodeTemplate/
  *
  */
